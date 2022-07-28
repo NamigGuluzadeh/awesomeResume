3b69eae659bc61b35c83b9bf9d95285bddb1efd3
Namig Guluzada <nquluzade95@gmail.com>
2022-07-28 22:20:34 +0400

    made a small change

--- src/AwesomeResume.java
+++ src/AwesomeResume.java
@@ -1,5 +1,6 @@
 public class AwesomeResume {
     public static void main(String[] args) {
         System.out.println("This is your resume");
+        System.out.println("another thing");
     }
 }
